2abd82539d7a64c6cc6dc7a8728c2756c168bcd6
Matthew Bailey <MBailey@jtafla.com>
2020-07-14 15:46:23 -0400

    Fixed EOD Bug

--- src/components/HOCs/withData.component.jsx
+++ src/components/HOCs/withData.component.jsx
@@ -9,6 +9,7 @@ import {newArrayfromIndexes} from "../../utils/array-functions"
 //NOTE: Break the sorting portion of this HOC up into a separate HOC Later!
 const WithData = (WrappedComponent, pageName, hasList) => {
     class WithData extends React.Component {
+        
         state={
             loadingStatus:0,
         }
