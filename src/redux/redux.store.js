384ef6a792b42aef7571d12c15da4504924d55fa
Matthew Bailey <MBailey@jtafla.com>
2020-07-15 20:32:42 -0400

    another fix for react not rendering issue

--- src/redux/redux.store.js
+++ src/redux/redux.store.js
@@ -1,3 +1,6 @@
+//NOTE: Aways remember to turn off redux debug middleware before deploying to prod!
+//=================================================================================
+
 import {createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers/root.reducer";
 import thunk from "redux-thunk";
@@ -7,10 +10,11 @@ const middleWare = [thunk];
 const store = createStore(
     rootReducer,
     {},
-    compose(
-        applyMiddleware(...middleWare),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
+    applyMiddleware(...middleWare)
+    // compose(
+    //     applyMiddleware(...middleWare),
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //   )
 );
 
 export default store;
